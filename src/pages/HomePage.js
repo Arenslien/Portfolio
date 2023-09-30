3769a35b4b8768a273db3fd4800bce2a3a529039
Arenslien <arenslien@gmail.com>
2023-09-30 20:52:25 +0900

    마우스 휠 기준 스크롤 함수 제작

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,55 +3,58 @@ import ThirdScreen from '../components/ThirdScreen';
 import HomeScreen from '../components/HomeScreen';
 import { useEffect, useRef, useState } from 'react';
 
-function HomePage() {
-  const [scrollDirection, setScrollDirection] = useState(null);
-  const [screenIndex, setScreenIndex] = useState(0);
+export default function HomePage() {
   const screenRef = useRef([]);
-  const delay = 500;
-
+  
+  // UseEffect 선언: []안의 값이 변경될 때 렌더링
   useEffect(() => {
-    const onWheel = (event) => {
-      console.log(event.deltaY);
-      if (event.deltaY > 0) {
-        setScrollDirection("down");
-      } else if (event.deltaY < 0) {
-        setScrollDirection("up");
-      } else {
-        setScrollDirection(null);
-      }
-      moveScreen(scrollDirection);
-      console.log(scrollDirection);
-    };
+    let scrollDirection = null;
+    let flag = false;
+    let screenIndex = 0;
 
-    const moveScreen = (scrollDirection) => {
+    const scrollScreen = (scrollDirection) => {
+      console.log("스크롤 함수의 방향 : " + scrollDirection);
       if (scrollDirection === "down" && screenIndex < 2) {
-        setScreenIndex(screenIndex + 1);
+        screenIndex++;
         screenRef.current[screenIndex].scrollIntoView({behavior: "smooth"});
       } else if (scrollDirection === "up" && screenIndex > 0) {
-        setScreenIndex(screenIndex - 1);
+        screenIndex--;
         screenRef.current[screenIndex].scrollIntoView({behavior: "smooth"});
       }
-    }
-    
-    // 스크롤 이벤트 리스너 추가
-    window.addEventListener("wheel", (event) => {
-      onWheel(event)
-      setTimeout(() => {
-      }, delay);
-    });
-    
+    };
 
-    // 컴포넌트가 언마운트될 대 이벤트 리스너 제거
+    const onWheel = (e) => {
+      // Atomic한 실행을 위한 flag 설정
+      if (!flag) {
+        // flag true로 설정
+        flag = true;
+        
+        // 1.1 wheel의 움직임에 맞게 스크롤 방향 설정
+        if (e.deltaY > 0) { // wheel down
+          scrollDirection = "down";
+        } else if (e.deltaY < 0) {
+          scrollDirection = "up";
+        }
+        console.log("설정된 방향 : " + scrollDirection);
+
+        // 1.2 방향에 맞게 스크롤
+        scrollScreen(scrollDirection);
+        setTimeout(() => {
+          flag = false;
+          console.log("flag false 설정");
+        }, 1000);
+      }
+
+    };
+
+    // 1. 마우스 휠에 대한 이벤트 리스너 등록
+    document.addEventListener("wheel", onWheel);
+
+    // 2. 이벤트 리스너 등록 헤제
     return () => {
-      window.removeEventListener("wheel", (event) => {
-        onWheel(event)
-        setTimeout(() => {    
-        }, delay);
-      });
+      document.removeEventListener("wheel", onWheel);
     }
-  }, [scrollDirection, screenIndex]);
-
-  
+  });
 
   return(
     <div>
@@ -61,5 +64,3 @@ function HomePage() {
     </div>
   );
 }
-
-export default HomePage;
